4199195c6c24b906ec4556de7b78dae906dcfc65
Pranjul <pranjul@Pranjuls-Mac-mini.local>
2025-09-03 16:10:42 +0530

    feat(internal): Added a basic config loading functions

---
+++ internal/config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Port      string
+	DbUrl     string
+	JwtSecret string
+}
+
+func LoadConfig() *Config {
+	_ = godotenv.Load()
+
+	cfg := &Config{
+		Port:      getEnv("PORT"),
+		DbUrl:     getEnv("DB_URL"),
+		JwtSecret: getEnv("JWT_SECRET"),
+	}
+
+	return cfg
+}
+
+func getEnv(key string) string {
+	val := os.Getenv(key)
+	if val == "" {
+		log.Fatalf("Error: Required Environment Variable %s, not set!", key)
+	}
+	return val
+}
