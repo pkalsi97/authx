98422d236bbefeeab141cf8974a1b76c1ef37154
Pranjul <pranjul@Pranjuls-Mac-mini.local>
2025-09-03 16:09:26 +0530

    feat(server): Added a basic server setup with db connection and gracefull start/stop

---
+++ cmd/server/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/pkalsi97/authx/api"
+	"github.com/pkalsi97/authx/internal/config"
+	"github.com/pkalsi97/authx/internal/db"
+)
+
+func main() {
+	cfg := config.LoadConfig()
+	port := cfg.Port
+	dbUrl := cfg.DbUrl
+
+	db.StartDb(dbUrl)
+
+	mux := api.SetUpRoutes()
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM, syscall.SIGTSTP)
+
+	go func() {
+		log.Printf(" Server running at :%s\n", port)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server Failed: %v", err)
+		}
+	}()
+
+	sig := <-sigChan
+	log.Printf("Shutdown signal received: %s", sig.String())
+
+	ctxShutdown, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctxShutdown); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	} else {
+		log.Println("Server exited gracefully")
+	}
+
+	db.CloseDB()
+}
