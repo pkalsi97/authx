6b48e3eab9e51c8f91aa15795b884a5b4f478d62
Pranjul <pranjul@Pranjuls-Mac-mini.local>
2025-09-03 16:08:12 +0530

    feat(api): Set up basic routing maps and endpoints

---
+++ api/routes.go
@@ -0,0 +1,76 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/pkalsi97/authx/internal/auth"
+	"github.com/pkalsi97/authx/internal/rbac"
+	"github.com/pkalsi97/authx/internal/user"
+)
+
+func SetUpRoutes() *http.ServeMux {
+	mux := http.NewServeMux()
+	registerUserPoolRoutes(mux)
+	registerAuthRoutes(mux)
+	registerOauthRoutes(mux)
+	registerUserRoutes(mux)
+	return mux
+}
+
+/*
+-----------------------
+
+	UserPool Routes
+
+-----------------------
+*/
+func registerUserPoolRoutes(mux *http.ServeMux) {
+	mux.HandleFunc("/api/v1/user-pools/", rbac.RbacRouter)
+}
+
+/*
+-----------------------
+
+	Auth Routes
+
+-----------------------
+*/
+func registerAuthRoutes(mux *http.ServeMux) {
+	mux.HandleFunc("/api/v1/auth/login/password", auth.PasswordLoginHandler)
+	mux.HandleFunc("/api/v1/auth/login/request", auth.LoginOtpRequestHandler)
+	mux.HandleFunc("/api/v1/auth/login/verify", auth.LoginOtpVerifyHandler)
+	mux.HandleFunc("/api/v1/auth/session/refresh", auth.SessionRefreshHandler)
+	mux.HandleFunc("/api/v1/auth/logout", auth.LogoutHandler)
+	mux.HandleFunc("/api/v1/auth/signup/otp/request", auth.SignupOtpRequestHandler)
+	mux.HandleFunc("/api/v1/auth/signup/otp/verify", auth.SignupOtpVerifyHandler)
+	mux.HandleFunc("/api/v1/auth/signup/complete", auth.SignupCompleteHandler)
+	mux.HandleFunc("/api/v1/auth/password/request", auth.PasswordResetRequestHandler)
+	mux.HandleFunc("/api/v1/auth/password/reset", auth.PasswordResetCompleteHandler)
+}
+
+/*
+---------------------
+
+	OAuth Routes
+
+-----------------------
+*/
+func registerOauthRoutes(mux *http.ServeMux) {
+	mux.HandleFunc("/api/v1/auth/oauth/google/redirect", auth.GoogleOauthRedirectHandler)
+	mux.HandleFunc("/api/v1/auth/oauth/google/callback", auth.GooleOauthCallbackHandler)
+	mux.HandleFunc("/api/v1/auth/oauth/git/redirect", auth.GitOauthRedirectHandler)
+	mux.HandleFunc("/api/v1/auth/oauth/git/callback", auth.GitOauthCallbackHandler)
+}
+
+/*
+-----------------------
+
+	User Routes
+
+-----------------------
+*/
+func registerUserRoutes(mux *http.ServeMux) {
+	mux.HandleFunc("/api/v1/users/password/reset", user.UserPasswordResetHandler)
+	mux.HandleFunc("/api/v1/users/credential/request", user.CredentialRequestHandler)
+	mux.HandleFunc("/api/v1/users/credential/verify", user.CredentialVerifyHandler)
+}
