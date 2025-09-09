96b7a31ebcc51c8b3d1e83c4a839a2c99d33f1ea
pkalsi97 <pranjulkalsi@gmail.com>
2025-09-09 16:12:46 +0530

    feat(utils): added a server logging middleware for request logging

---
+++ internal/utils/loggingmiddleware.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type statusWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		ww := &statusWriter{ResponseWriter: w, statusCode: http.StatusOK}
+
+		next.ServeHTTP(ww, r)
+		log.Printf("%s %s %d %s",
+			r.Method,
+			r.URL.Path,
+			ww.statusCode,
+			time.Since(start),
+		)
+	})
+}
+
+func (w *statusWriter) WriteHeader(code int) {
+	w.statusCode = code
+	w.ResponseWriter.WriteHeader(code)
+}
