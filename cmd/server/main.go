acd65195472e80ab2009d66ded4699a912be49e4
pkalsi97 <pranjulkalsi@gmail.com>
2025-09-09 16:32:57 +0530

    chore(server): Added swagger docs in main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -15,6 +15,21 @@ import (
 	"github.com/pkalsi97/authx/internal/utils"
 )
 
+// @title           AuthX API
+// @version         1.0
+// @description     Authentication and credential management APIs.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:3000
+// @BasePath  /api/v1
+// @schemes   http https
 func main() {
 	cfg := config.LoadConfig()
 	port := cfg.Port
