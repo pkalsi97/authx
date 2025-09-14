bd7290f86eecf26d4fd945029f4890195e9793b5
pkalsi97 <pranjulkalsi@gmail.com>
2025-09-14 15:53:42 +0530

    feat(core): Add migration function to take care of running migrations

---
+++ internal/core/migrations.go
@@ -0,0 +1,25 @@
+package core
+
+import (
+	"log"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func RunMigrations(databaseURL string) {
+	m, err := migrate.New(
+		"file://migrations",
+		databaseURL,
+	)
+	if err != nil {
+		log.Fatalf("Failed to initialize migrations: %v", err)
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatalf("Failed to apply migrations: %v", err)
+	}
+
+	log.Println("Migrations applied successfully")
+}
